ee60f5ce6b404028987c30bea1dfc573b7acf45b
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-14 00:16:19 +1200

    created model section for the data Grocery

--- ShoppingListAPI2/Models/Grocery.cs
+++ ShoppingListAPI2/Models/Grocery.cs
@@ -7,5 +7,8 @@ namespace ShoppingListAPI2.Models
 {
     public class Grocery
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool Purchased { get; set; }
     }
 }
