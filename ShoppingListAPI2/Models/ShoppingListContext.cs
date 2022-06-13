f8ae7b5d248ac059466f874e45ee10e718972b1f
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2022-06-14 00:22:15 +1200

    Setting up the dbcontext

---
+++ ShoppingListAPI2/Models/ShoppingListContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingListAPI2.Models
+{
+    public class ShoppingListContext : DbContext
+    {
+        public ShoppingListContext(DbContextOptions<ShoppingListContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Grocery> Groceries { get; set; }
+    }
+}
